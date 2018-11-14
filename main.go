8fa2233987ec2392afbe11570f4ce2e6d3fb7bbb
milanverespej <44735530+milanverespej@users.noreply.github.com>
2018-11-14 12:27:59 +0100

    Fix imports

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "sync"
-    "github.com/milanverespej/lunch_info/basen"
-    "github.com/milanverespej/lunch_info/eurest"
+	"github.com/milanverespej/lunch-info/basen"
+	"github.com/milanverespej/lunch-info/eurest"
+	"sync"
 )
 
 func main() {
-    var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-    wg.Add(2)
+	wg.Add(2)
 
-    go func() {
-        basen.DailyMenu()
-        defer wg.Done()
-    }()
-     go func() {
-        eurest.DailyMenu()
-        defer wg.Done()
-    }()
+	go func() {
+		basen.DailyMenu()
+		defer wg.Done()
+	}()
+	go func() {
+		eurest.DailyMenu()
+		defer wg.Done()
+	}()
 
-    wg.Wait()
+	wg.Wait()
 }
